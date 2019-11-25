c8b5862aa3de3a380400c3714de78e0c9893728a
Théo Malaper <theomalaper.cognez@gmail.com>
2019-11-25 12:21:13 -1000

    started nav barimplementation and added icons svg

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Homepage from './Homepage'
 import useApplicationData from '../hooks/useApplicationData'
@@ -11,7 +11,17 @@ export default function App() {
   return (
     <Router>
       <div>
-        <header></header>
+        <header className="App-Nav-bar">
+          <section className="Nav-bar-left">
+            <h3>Biscuiterie - Crêperie de Tahiti</h3>
+          </section>
+          <section className="Nav-bar-right">
+            <Link className="Nav-bar-links react-links" to="/">Processus</Link>
+            <Link className="Nav-bar-links react-links" to="/">Gamme</Link>
+            <Link className="Nav-bar-links react-links" to="/">A propos</Link>
+            <Link className="Nav-bar-links react-links" to="/contact">Contact</Link>
+          </section>
+        </header>
         <Switch>
           <Route exact path="/">
             <Homepage 
