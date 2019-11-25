9d1d742549fa56b1d0022a7b1dcf05e844e25d5d
Théo Malaper <theomalaper.cognez@gmail.com>
2019-11-25 10:31:09 -1000

    finished set up of the client and added useApplication and useReducer hook

---
+++ client/src/hooks/useApplicationData.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+const SET_HOMEPAGE_DATA = 'SET_HOMEPAGE';
+
+const reducer = (state, action) => {
+  const actions = {
+    SET_HOMEPAGE_DATA: {
+      ...state,
+      products: action.products
+    }
+  }; 
+
+  if(!actions[action.type]) {
+    throw new Error('Type of action not found')
+  };
+  return actions[action.type];
+};
+
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    products: {},
+    product_types: {},
+  })
+
+  useEffect(() => {
+    axios.get('/products')
+      .then(result => {
+        dispatch({ type: SET_HOMEPAGE_DATA, products: result.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  return {
+    state,
+    dispatch
+  };
+};
+
+export default useApplicationData;
