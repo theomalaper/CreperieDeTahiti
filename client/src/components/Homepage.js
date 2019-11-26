00e401f322634d2ee4ed7ae40f92010bd896e65d
Théo Malaper <theomalaper.cognez@gmail.com>
2019-11-25 15:39:45 -1000

    implemented landing page design on the homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,6 +3,16 @@ import './Homepage.scss';
 
 export default function Homepage() {
   return (
-    <p>Hello</p>
+    <section className="landing-page">
+      <div className="landing-page--background">
+        <img className="landing-background-image" src="https://github.com/theomalaper/CreperieDeTahiti/blob/master/doc/crepe-orange.jpg?raw=true" alt="Landing Page Background"/>
+        <div className="landing-overlay"></div>
+      </div>
+      <div className="landing-page--presentation">
+        <h1 className="landing-title">Experience de produits de qualité et naturel</h1>
+        <p className="landing-paragraph">Vous cherchez des produits de qualité, frais, produits au fenua et completement naturel contactze nous des maintenant pour voir ce que l’on peut faire pour vous</p>
+        <button className="landing-contact-button"><p>CONTACTEZ NOUS</p></button>
+      </div>
+    </section>
   );
 };
